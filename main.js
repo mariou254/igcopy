f8df5dc17677591fe41da0b39463eed2f098f3ba
Mario Alexis Uribe <umario284@gmail.com>
2022-05-19 17:23:53 -0500

    mod-bot

--- main.js
+++ main.js
@@ -3,4 +3,5 @@ ScrollReveal().reveal('.row img', { delay: 1400 });
 ScrollReveal().reveal('.status', { delay: 100 });
 ScrollReveal().reveal('.status .status-card', { delay: 500 });
 ScrollReveal().reveal('.pres-1 img', { delay: 500 });
-ScrollReveal().reveal('footer',{delay:500});
+ScrollReveal().reveal('footer', { delay: 500 });
+ScrollReveal().reveal('.etiq', { delay: 500 });
